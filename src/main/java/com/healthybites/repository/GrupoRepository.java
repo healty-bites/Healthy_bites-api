3633b6ed5d5b1fb208762bbd1b1fbff6b0e3cdb9
Valentin Emanuel Fernandez Gutierrez <valenferxux@gmail.com>
2024-09-28 16:22:43 -0500

    feat(backend): implementacion del CRUD en grupo

---
+++ src/main/java/com/healthybites/repository/GrupoRepository.java
@@ -0,0 +1,7 @@
+package com.healthybites.repository;
+
+import com.healthybites.model.entity.Grupo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
+}

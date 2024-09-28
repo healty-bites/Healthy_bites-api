10cf970d9efbdba129b7d1c22c3d1d19844c6177
Valentin Emanuel Fernandez Gutierrez <valenferxux@gmail.com>
2024-09-28 17:01:22 -0500

    feat(backend): implementacion del CRUD en plan alimenticio

---
+++ src/main/java/com/healthybites/repository/PlanAlimenticioRepository.java
@@ -0,0 +1,7 @@
+package com.healthybites.repository;
+
+import com.healthybites.model.entity.PlanAlimenticio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanAlimenticioRepository extends JpaRepository<PlanAlimenticio, Integer> {
+}
